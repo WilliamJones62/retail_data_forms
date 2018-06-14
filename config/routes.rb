5bdabf0e49e35c07630865a7abb71e9a72836662
William Jones <williamjones120862@gmail.com>
2018-06-14 11:26:57 -0400

    add a home page and a navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/show'
+
   resources :calllists
   resources :authorlists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#show'
 end
