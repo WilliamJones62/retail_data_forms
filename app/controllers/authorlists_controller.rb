ce2fa5a3078da1dcad91e005e1923f4f7e491acf
William Jones <williamjones120862@gmail.com>
2018-08-15 07:23:24 -0400

    display author list by customer

--- app/controllers/authorlists_controller.rb
+++ app/controllers/authorlists_controller.rb
@@ -1,10 +1,10 @@
 class AuthorlistsController < ApplicationController
   before_action :set_authorlist, only: [:show, :edit, :update, :destroy]
+  before_action :build_customer_list, only: [:index]
 
   # GET /authorlists
   # GET /authorlists.json
   def index
-    @authorlists = Authorlist.all
   end
 
   # GET /authorlists/1
@@ -29,10 +29,8 @@ class AuthorlistsController < ApplicationController
     respond_to do |format|
       if @authorlist.save
         format.html { redirect_to @authorlist, notice: 'Authorlist was successfully created.' }
-        format.json { render :show, status: :created, location: @authorlist }
       else
         format.html { render :new }
-        format.json { render json: @authorlist.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +41,8 @@ class AuthorlistsController < ApplicationController
     respond_to do |format|
       if @authorlist.update(authorlist_params)
         format.html { redirect_to @authorlist, notice: 'Authorlist was successfully updated.' }
-        format.json { render :show, status: :ok, location: @authorlist }
       else
         format.html { render :edit }
-        format.json { render json: @authorlist.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,8 +53,21 @@ class AuthorlistsController < ApplicationController
     @authorlist.destroy
     respond_to do |format|
       format.html { redirect_to authorlists_url, notice: 'Authorlist was successfully destroyed.' }
-      format.json { head :no_content }
     end
+  end
+
+  def selected
+    customers = []
+    customers = $author_customers
+    customers.each do |c|
+      select_item = []
+      select_item = c
+      if params[:author_custcode] == select_item[1].to_s
+        session[:author_custcode] = select_item[0]
+        break
+      end
+    end
+    redirect_to action: "index"
   end
 
   private
@@ -67,8 +76,51 @@ class AuthorlistsController < ApplicationController
       @authorlist = Authorlist.find(params[:id])
     end
 
+    # Build a list of current customers
+    def build_customer_list
+      @custcode = []
+      custcode = []
+      tempcust = []
+      authorlist = Authorlist.all
+      authorlist.each do |a|
+        if !tempcust.include?(a.custcode)
+          tempcust.push(a.custcode)
+        end
+      end
+      custcode = tempcust.sort
+
+      if !session[:author_custcode] || session[:author_custcode == '']
+        # not returning from update of called flag
+        session[:author_custcode] = custcode[0]
+      end
+
+      i = 1
+      @selected_customer = 0
+      custcode.each do |c|
+        select_item = []
+        select_item.push(c)
+        select_item.push(i)
+        @custcode.push(select_item)
+        if c == session[:author_custcode]
+          @selected_customer = i
+        end
+        i += 1
+      end
+      $author_customers = @custcode
+
+    # Set up the author list for the first customer or the returning customer
+      @authorlists = []
+
+      authorlist.each do |c|
+        if c.custcode and c.custcode == session[:author_custcode]
+          # include author list records that are a direct match for customer
+          @authorlists.push(c)
+        end
+      end
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def authorlist_params
-      params.require(:authorlist).permit(:partcode, :dept, :custcode, :turns, :seq, :priority)
+      params.require(:authorlist).permit(:partcode, :dept, :custcode, :turns, :seq, :priority, :author_custcode)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :calllists do
     collection { post :import }
   end
+  get 'authorlists/selected'
   resources :authorlists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#show'
